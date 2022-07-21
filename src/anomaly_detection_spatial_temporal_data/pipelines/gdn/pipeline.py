b1432cf1c05b43fee7ec54f6679fa5d6fec92727
EC2 Default User <ec2-user@ip-172-16-174-42.ec2.internal>
2022-07-21 00:51:06 +0000

    add wifi pipelines

--- src/anomaly_detection_spatial_temporal_data/pipelines/gdn/pipeline.py
+++ src/anomaly_detection_spatial_temporal_data/pipelines/gdn/pipeline.py
@@ -12,9 +12,17 @@ def create_pipeline(**kwargs) -> Pipeline:
         [
             node(
                 func=set_and_train_model,
-                inputs=["iot_sensor_cols_txt", "iot_data_gdn_train", "iot_data_gdn_test", "params:train_config", "params:env_config"],
+                inputs=["iot_sensor_cols_txt", "iot_data_gdn_train", "iot_data_gdn_test", "params:train_config", "params:env_config_iot"],
                 outputs=None,
-                name="train_gdn"
+                name="train_gdn_iot",
+                tags="iot_gdn"
+            ),
+            node(
+                func=set_and_train_model,
+                inputs=["wifi_sensor_cols_txt", "wifi_data_gdn_train", "wifi_data_gdn_test", "params:train_config", "params:env_config_wifi"],
+                outputs=None,
+                name="train_gdn_wifi",
+                tags="wifi_gdn"
             )
         ]
     )
